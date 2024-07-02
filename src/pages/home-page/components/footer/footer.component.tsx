89a54098bc247286fe02741e45b640c3ad1b921a
Skrexz <xsebastianrojasx@gmail.com>
2024-07-01 22:00:11 -0500

    Cambios de textos

--- src/pages/home-page/components/footer/footer.component.tsx
+++ src/pages/home-page/components/footer/footer.component.tsx
@@ -7,9 +7,10 @@ const FooterComponent = () => {
 		<div className={styles.container}>
 			<span className={styles.title}>SoloLashes</span>
 			<p className={styles.paragraph}>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-				debitis quod quis, facere minus mollitia, commodi nulla laudantium
-				repellendus ab nihil aliquid?
+			Orlando Lash Lady is known for her customized design and detailed work.
+			 She founded her business with one goal in mind - to make lash services customizable,
+			  safe and affordable for every woman and man. Her Lash Extensions Services focuses on safety,
+			   beauty and quality results. Ready to embark on a beautiful journey?
 			</p>
 			<div className={styles.gridContainer}>
 				<div className={styles.gridItem}>
