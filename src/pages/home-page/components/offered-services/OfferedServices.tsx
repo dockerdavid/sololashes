0dc395f9895ff6edac0766028983cbc548ed224e
Duban Caballero <duban.caballero@amovil.co>
2024-07-21 18:02:11 -0500

    fix styles

--- src/pages/home-page/components/offered-services/OfferedServices.tsx
+++ src/pages/home-page/components/offered-services/OfferedServices.tsx
@@ -2,23 +2,23 @@ import styles from "./OfferedServices.module.css";
 import data from "../../../../data/offered-services/data.json";
 
 export const OfferedServices = () => {
-  return data.sections.map((service, index) => (
-    <div
-      key={index}
-      style={{
-        backgroundColor: service["background-color"],
-      }}
-      className={styles.offeredServicesContainer}
-    >
-      <div className={styles.offeredServicesinformation}>
-        <h2 style={{ color: service["title-color"] }}>{service.title}</h2>
-        <span style={{ color: service["subtitle-color"] }}>{service.subtitle}</span>
-        <span style={{ color: service["body-color"] }}>{service.body}</span>
-        <span style={{ color: service["pricing-color"] }}>{service.pricing}</span>
-      </div>
-      <div className={styles.offeredServicesimage}>
-        <img src={service.image} />
-      </div>
-    </div>
-  ));
+	return data.sections.map((service, index) => (
+		<div
+			key={index}
+			style={{
+				backgroundColor: service["background-color"],
+			}}
+			className={`${styles.offeredServicesContainer} ${index % 2 === 0 ? styles.offeredServicesContainerOdd : styles.offeredServicesContainerEven}`}
+		>
+			<div className={styles.offeredServicesinformation}>
+				<h2 style={{ color: service["title-color"] }}>{service.title}</h2>
+				<span style={{ color: service["subtitle-color"] }}>{service.subtitle}</span>
+				<span style={{ color: service["body-color"] }}>{service.body}</span>
+				<span style={{ color: service["pricing-color"] }}>{service.pricing}</span>
+			</div>
+			<div className={styles.offeredServicesimage}>
+				<img src={service.image} />
+			</div>
+		</div>
+	));
 };
