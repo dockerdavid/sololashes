64e00a6b93ab558e379e81c63e38330683b2f6e7
David Gomez <david.gomez@amovil.co>
2024-07-25 19:13:11 -0500

    feat: Actualizar imágenes y estilos del slider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./index.css";
 
 import App from "./App";
