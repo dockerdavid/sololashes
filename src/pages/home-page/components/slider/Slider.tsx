ad4f123e96987c54d127cc8605b2db7c25839e39
David Gomez <david.gomez@amovil.co>
2024-07-25 19:10:53 -0500

    feat: Actualizar imágenes y estilos del slider

--- src/pages/home-page/components/slider/Slider.tsx
+++ src/pages/home-page/components/slider/Slider.tsx
@@ -1,36 +1,18 @@
 import styles from "./Slider.module.css";
 import images from "../../../../data/slider/data.json";
-import { Carousel } from "react-responsive-carousel";
 
 export const Slider = () => {
   return (
-    <div>
-      <Carousel
-        autoPlay
-        infiniteLoop
-        showThumbs={false}
-        showIndicators={false}
-        preventMovementUntilSwipeScrollTolerance
-        swipeScrollTolerance={50}
-        showArrows={false}
-        showStatus={false}
-        interval={4500}
-
-      >
-        {images.images.map((image, index) => (
-          <div
-            key={index}
-            className={styles.imageContainerSlider}
-            style={{ height: "300px", width: "100%" }}
-          >
-            <img src={image.image} alt={image.text} />
-            <div className={styles.textContainerSlider}>
-              <p className={styles.text}>{image.text}</p>
-              <p className={styles.subtitle}>{image.subtitle}</p>
-            </div>
+    <div className={styles.imageContainerSlider}>
+      {images.images.map((image) => (
+        <div className={styles.cardSliderContainer}>
+          <img src={image.image} alt="" className={styles.cardImage} />
+          <div className={styles.cardSliderText}>
+            <span className={styles.mainText}>{image.text}</span><br />
+            <span className={styles.subtitleText}>{image.subtitle}</span>
           </div>
-        ))}
-      </Carousel>
+        </div>
+      ))}
     </div>
   );
 };
